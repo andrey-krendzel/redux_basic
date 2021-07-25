c91a37a26b7a71aef7a46b31a8b1f945f09c8cca
unknown <andreykrendzel@hotmail.com>
2021-07-25 09:18:24 +0100

    some updateS

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -36,16 +36,12 @@ const noteReducer = (state = [], action) => {
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
 
-export const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
+  export const createNote = (data) => {
+    return {
+      type: 'NEW_NOTE',
+      data,
     }
   }
-}
 
 export const toggleImportanceOf = (id) => {
   return {
