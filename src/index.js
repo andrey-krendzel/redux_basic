92fb785a32ee5d8345bcd5609e161e9f8a9e271b
unknown <andreykrendzel@hotmail.com>
2021-07-26 19:38:27 +0100

    added todays changes

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
 
-import filterReducer from './reducers/filterReducer'
-
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
-
-import noteService from './services/notes'
-
-import noteReducer, { initializeNotes } from './reducers/noteReducer'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer, composeWithDevTools())
-
-noteService.getAll().then(notes =>
-  store.dispatch(initializeNotes(notes))
-)
-
-
-console.log(store.getState())
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.render(
   
